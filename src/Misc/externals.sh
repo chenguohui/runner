7d7cba3fa7ffefbf3dfed6ac2568be44c2cd38e1
chenguohui <chenguohui@loongson.cn>
2025-12-04 09:39:34 +0800

    Release linux-loongarch64 runner v2.330.0

--- src/Misc/externals.sh
+++ src/Misc/externals.sh
@@ -3,11 +3,13 @@ PACKAGERUNTIME=$1
 PRECACHE=$2
 
 NODE_URL=https://nodejs.org/dist
+UNOFFICIAL_NODE_URL=https://unofficial-builds.nodejs.org/download/release
 NODE_ALPINE_URL=https://github.com/actions/alpine_nodejs/releases/download
 # When you update Node versions you must also create a new release of alpine_nodejs at that updated version.
 # Follow the instructions here: https://github.com/actions/alpine_nodejs?tab=readme-ov-file#getting-started
 NODE20_VERSION="20.19.5"
 NODE24_VERSION="24.11.1"
+NODE20_UNOFFICIAL_VERSION="20.16.0"
 
 get_abs_path() {
   # exploits the fact that pwd will print abs path when no args
@@ -186,4 +188,8 @@ fi
 
 if [[ "$PACKAGERUNTIME" == "linux-arm" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE20_VERSION}/node-v${NODE20_VERSION}-linux-armv7l.tar.gz" node20 fix_nested_dir
+fi
+
+if [[ "$PACKAGERUNTIME" == "linux-loongarch64" ]]; then
+    acquireExternalTool "$UNOFFICIAL_NODE_URL/v${NODE20_UNOFFICIAL_VERSION}/node-v${NODE20_UNOFFICIAL_VERSION}-linux-loong64.tar.gz" node20 fix_nested_dir
 fi
